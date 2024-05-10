423a966d627cd94b0c6f24ccaa42624c0737fd5c
tyq-ext-pc <275553824@qq.com>
2024-05-10 11:25:23 +0800

    修改运行方法，采用exec_command 的方式，另外通过管道的方式运行一条命令简化代码

--- model/SysPwdModify/SysPwdModify.py
+++ model/SysPwdModify/SysPwdModify.py
@@ -1,103 +1,39 @@
 # -*- coding: utf-8 -*-
 import os
-
-import chardet
-
-# from lib.tools import Tools
-# import paramiko
-import time
 import json
 from lib.readcfg import ReadCfg
-
-
-def ssh_reply(chanel_recv):
-    whcode = chardet.detect(chanel_recv)['encoding']
-    reply = chanel_recv.decode(whcode, 'ignore')
-    return reply
 
 
 class ModelClass(object):
     def __init__(self, mylog):
         self.mylog = mylog
 
-    def shellCommand(self, ssh, command, stdinfo, timeout=10):
-        channel = ssh.invoke_shell()
-        channel.send("export LANG=en_US.UTF-8 \n")
-        time.sleep(0.01)
-        channel.send(command + "\n")
-        time.sleep(0.05)
-
-        return_bool = True
-        buff = ""
-        i = 0
-        self.mylog.debug("开始查找assword:")
-        while not (buff.endswith("assword: ") or buff.endswith("密码：")):
-            self.mylog.debug("查找assword:...")
-            time.sleep(0.1)
-            resp = channel.recv(9999)
-            reply = ssh_reply(resp)
-            buff += reply
-            i = i + 1
-            while i > 50:
-                break
-
-        self.mylog.debug("输入信息")
-        for info in stdinfo:
-            while not channel.send_ready():
-                time.sleep(0.009)
-            channel.send(info)
-            channel.send("\n")
-            self.mylog.debug(f"输入:{info}")
-            time.sleep(0.2)
-
-        buff = ""
-
-        self.mylog.debug("开始查找$ ")
-        base_time = time.time()
-        while not buff.endswith("$ "):
-            while not channel.recv_ready():
-                self.mylog.debug("sleep 0.9")
-                time.sleep(0.9)
-                if time.time() >= (base_time + timeout):
-                    return [True, f"channel.recv_ready time out, buff:{buff}"]
-            self.mylog.debug("channel.recv(9999)")
-            resp = channel.recv(9999)
-            reply = ssh_reply(resp)
-            buff += reply
-            self.mylog.debug(f"reply:{reply}")
-            if "BAD PASSWORD" in buff:
-                return_bool = False
-                break
-
-        self.mylog.debug(f"buff:{buff}")
-        return [return_bool, buff]
-
     def modifyNewPassword(self, hostname, ssh, password, newpassword):
-        stdinfo = [password, newpassword, newpassword]
-        rs = self.shellCommand(ssh, "passwd", stdinfo)
+        cmd = f"echo -e '{password}\n{newpassword}\n{newpassword}\n' | passwd"
+        stdin, stdout, stderr = ssh.exec_command(cmd)
 
-        if type(rs[1]) == str:
-            self.mylog.debug(f"rs1内容：{rs[1]}")
-            if "successfully" in rs[1] or "成功" in rs[1]:
-                self.mylog.info("修改密码：主机{}修改成功".format(hostname))
-            elif "manipulation error" in rs[1]:
-                self.mylog.error(f"修改密码：主机{hostname}修改失败,当前密码错误,{rs[1]}")
-                raise Exception("修改密码：主机{}修改失败,当前密码错误".format(hostname))
-            elif "BAD PASSWORD" in rs[1]:
-                self.mylog.error(f"修改密码：主机{hostname}修改失败,新密码不符合要求,{rs[1]}")
-                raise Exception("修改密码：主机{}修改失败,新密码不符合要求".format(hostname))
-            else:
-                self.mylog.error(f"修改密码：主机{hostname}修改失败,{rs[1]}")
-                raise Exception("修改密码：主机{}修改失败".format(hostname))
+        # 读取命令执行结果
+        output = stdout.read().decode('utf-8')
+        error = stderr.read().decode('utf-8')
+
+        if stderr.read().decode():
+            self.mylog.error(f"修改密码：修改失败-{error}")
+        else:
+            self.mylog.info(f"修改密码：修改成功-{output}")
 
     def checkwd(self, hostname, ssh, username, password):
-        rs = self.shellCommand(ssh, f"su - {username}", [password])
+        cmd = f"echo '{password}' | su - {username}"
+        stdin, stdout, stderr = ssh.exec_command(cmd)
 
-        if type(rs[1]) == str:
-            if "Last login" in rs[1] or "上一次登录" in rs[1]:
-                self.mylog.info("检查：主机{}登录检查成功".format(hostname))
-            else:
-                self.mylog.error(f"检查：主机{hostname}登录检查失败,{rs[1]}")
+        # 读取命令执行结果
+        output = stdout.read().decode('utf-8')
+        error = stderr.read().decode('utf-8')
+
+        # 输出命令执行结果
+        if error:
+            self.mylog.error(f"检查：主机登录检查失败-{error}")
+        else:
+            self.mylog.info(f"检查：主机登录检查成功-{output}")
 
     def action(self, ssh, hostname, param, hostparam=None):
         if hostparam is None:
