53bbcae061fd08b01d43c326bdf7eaa3e6fe191b
tyq-ext-pc <275553824@qq.com>
2024-06-20 11:19:43 +0800

    增加参数判断，默认参数不用硬编码

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 # 这是一个示例 Python 脚本。
 import argparse
 import codecs
+import os
+
 codecs.register(lambda name: codecs.lookup('utf-8') if name == 'idna' else None)
 
 from Tansible import Tansible
-
+import lib.constants as C
 
 def args_fun():
     parser = argparse.ArgumentParser(description='Tansible command line tool')
-    parser.add_argument('-a', '--action_file', type=str, default='config/action.yaml',
+    parser.add_argument('-a', '--action_file', type=str, default=C.DEFAULT_ACTION_FILE,
                         help='input the action file name,like action.yml')
-    parser.add_argument('-ho ', '--hosts_config_file', type=str, default='config/hosts.yaml',
+    parser.add_argument('-ho ', '--hosts_config_file', type=str, default=C.DEFAULT_HOSTS_FILE,
                         help='input hosts file name,default is  hosts.yml')
-    parser.add_argument('-g ', '--groups_config_file', type=str, default='config/groups.yaml',
+    parser.add_argument('-g ', '--groups_config_file', type=str, default=C.DEFAULT_GROUPS_FILE,
                         help='input groups file name,default is  groups.yml')
     parser.add_argument('-s ', '--run_step_by_step', action='store_const', const=True,
                         help='enable step by step mode, only run with single thread and only for debug')
@@ -21,10 +23,23 @@ def args_fun():
     args = parser.parse_args()
     return args
 
+# 参数判断
+def args_check(args):
+    if not os.path.exists(args.action_file):
+        print(f"action file {args.action_file} not exist")
+        exit(1)
+    if not os.path.exists(args.hosts_config_file):
+        print(f"hosts file {args.hosts_config_file} not exist")
+        exit(1)
+    # groups文件如果配置为非默认值，则必须存在，否则报错
+    if args.groups_config_file != C.DEFAULT_GROUPS_FILE and not os.path.exists(args.groups_config_file):
+        print(f"groups file {args.groups_config_file} not exist")
+        exit(1)
 
 def work():
     args = args_fun()
     print(args)
+    args_check(args)
     obj = Tansible(actions_file=args.action_file, hosts_file=args.hosts_config_file,
                    groups_file=args.groups_config_file, max_workers=args.workers, step_by_step=args.run_step_by_step)
     obj.action_func()
