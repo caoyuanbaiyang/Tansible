de8742c61ee072b68d56e92971bddbf2de2ae6a8
tyq-ext-pc <275553824@qq.com>
2024-10-30 11:31:49 +0800

    删除多余空格

--- main.py
+++ main.py
@@ -12,13 +12,13 @@ def args_fun():
     parser = argparse.ArgumentParser(description='Tansible command line tool')
     parser.add_argument('-a', '--action_file', type=str, default=C.DEFAULT_ACTION_FILE,
                         help='input the action file name,like action.yml')
-    parser.add_argument('-ho ', '--hosts_config_file', type=str, default=C.DEFAULT_HOSTS_FILE,
+    parser.add_argument('-ho', '--hosts_config_file', type=str, default=C.DEFAULT_HOSTS_FILE,
                         help='input hosts file name,default is  hosts.yml')
-    parser.add_argument('-g ', '--groups_config_file', type=str, default=C.DEFAULT_GROUPS_FILE,
+    parser.add_argument('-g', '--groups_config_file', type=str, default=C.DEFAULT_GROUPS_FILE,
                         help='input groups file name,default is  groups.yml')
-    parser.add_argument('-s ', '--run_step_by_step', action='store_const', const=True,
+    parser.add_argument('-s', '--run_step_by_step', action='store_const', const=True,
                         help='enable step by step mode, only run with single thread and only for debug')
-    parser.add_argument('-w ', '--workers', type=int,
+    parser.add_argument('-w', '--workers', type=int,
                         help='run with multi thread,you can set the number of threads here or in the action file')
     args = parser.parse_args()
     return args
