5c9701811094ec456a67d37518d08ac24ce6224b
tyq-ext-pc <275553824@qq.com>
2025-04-09 11:00:18 +0800

    开用版本，但是对输入错误没有检验

--- app.py
+++ app.py
@@ -1,58 +1,78 @@
 import os
 import threading
 import webbrowser
+from flask import Flask, render_template, request, session, jsonify
 
-from flask import Flask, render_template, request, session
 from Tansible import Tansible
 from lib.constants import DEFAULT_HOSTS_FILE, DEFAULT_ACTION_FILE, DEFAULT_GROUPS_FILE
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'  # 设置密钥，用于加密会话数据
+app.secret_key = 'your_secret_key'
 
 def args_check(action_file, hosts_config_file, groups_config_file, workers, run_step_by_step=False):
-    error_messages = []
+    error_messages = {
+        'action_file': '',
+        'hosts_config_file': '',
+        'groups_config_file': '',
+        'workers': ''
+    }
     if not os.path.exists(action_file):
-        error_messages.append(f"action file {action_file} not exist")
+        error_messages['action_file'] = f"action file {action_file} not exist"
     if not os.path.exists(hosts_config_file):
-        error_messages.append(f"hosts file {hosts_config_file} not exist")
-    # groups文件如果配置为非默认值，则必须存在，否则报错
+        error_messages['hosts_config_file'] = f"hosts file {hosts_config_file} not exist"
     if groups_config_file != DEFAULT_GROUPS_FILE and not os.path.exists(groups_config_file):
-        error_messages.append(f"groups file {groups_config_file} not exist")
-    # -s 选项只能在单线程模式下使用
-    if run_step_by_step and workers > 1 :
-        error_messages.append("-s option only can be used in single thread mode")
+        error_messages['groups_config_file'] = f"groups file {groups_config_file} not exist"
+    if run_step_by_step and workers > 1:
+        error_messages['workers'] = "-s option only can be used in single thread mode"
     return error_messages
+
+def get_directory_tree(path):
+    tree = []
+    for item in os.listdir(path):
+        item_path = os.path.join(path, item)
+        if os.path.isdir(item_path):
+            subtree = {
+                'name': item,
+                'type': 'directory',
+                'children': get_directory_tree(item_path)
+            }
+            tree.append(subtree)
+        else:
+            tree.append({
+                'name': item,
+                'type': 'file'
+            })
+    return tree
+
+@app.route('/get_config_tree', methods=['GET'])
+def get_config_tree():
+    config_path = '.'  # 假设 config 目录在项目根目录下
+    tree = get_directory_tree(config_path)
+    return jsonify(tree)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        # 获取输入的文件路径
         host_file_path = request.form.get('host_file_path', DEFAULT_HOSTS_FILE)
         group_file_path = request.form.get('group_file_path', DEFAULT_GROUPS_FILE)
         action_file_path = request.form.get('action_file_path', DEFAULT_ACTION_FILE)
-        # 获取并发线程数
         max_workers = request.form.get('max_workers')
 
-        # 将输入信息存储到会话中
         session['host_file_path'] = host_file_path
         session['group_file_path'] = group_file_path
         session['action_file_path'] = action_file_path
         session['max_workers'] = max_workers
 
-        # 校验输入的值
         try:
             max_workers = int(max_workers) if max_workers else 1
             error_messages = args_check(action_file_path, host_file_path, group_file_path, max_workers)
-            if error_messages:
-                error_message = ", ".join(error_messages)
-                return render_template('result.html', error=error_message, host_file_path=host_file_path,
-                                       group_file_path=group_file_path, action_file_path=action_file_path,
-                                       max_workers=max_workers)
+            if any(error_messages.values()):
+                session['error_messages'] = error_messages
+                return index()
         except ValueError as e:
             error_message = str(e)
-            return render_template('result.html', error=error_message, host_file_path=host_file_path,
-                                   group_file_path=group_file_path, action_file_path=action_file_path,
-                                   max_workers=max_workers)
+            session['error_messages'] = {'workers': error_message}
+            return index()
 
         if host_file_path and group_file_path and action_file_path:
             try:
@@ -62,32 +82,33 @@ def index():
                     groups_file=group_file_path,
                     max_workers=max_workers
                 )
-                # 模拟执行任务
                 tansible.action_func()
                 result = tansible.result
-                return render_template('result.html', result=result, host_file_path=host_file_path,
-                                       group_file_path=group_file_path, action_file_path=action_file_path,
-                                       max_workers=max_workers)
+                return render_template('result.html', result=result)
             except Exception as e:
                 error_message = f"Tansible 任务执行失败，错误信息：{str(e)}"
-                return render_template('result.html', error=error_message, host_file_path=host_file_path,
-                                       group_file_path=group_file_path, action_file_path=action_file_path,
-                                       max_workers=max_workers)
+                return render_template('result.html', error=error_message)
         else:
             error_message = "请输入所有必要的文件路径。"
-            return render_template('result.html', error=error_message, host_file_path=host_file_path,
-                                   group_file_path=group_file_path, action_file_path=action_file_path,
-                                   max_workers=max_workers)
+            return render_template('result.html', error=error_message)
 
-    # 从会话中获取之前输入的信息，如果没有则使用默认值
     host_file_path = session.get('host_file_path', DEFAULT_HOSTS_FILE)
     group_file_path = session.get('group_file_path', DEFAULT_GROUPS_FILE)
     action_file_path = session.get('action_file_path', DEFAULT_ACTION_FILE)
-    max_workers = session.get('max_workers')
+    max_workers = session.get('max_workers', 1)
+    error_messages = session.pop('error_messages', {})
 
-    return render_template('index.html', default_hosts_file=DEFAULT_HOSTS_FILE, default_action_file=DEFAULT_ACTION_FILE,
-                           default_groups_file=DEFAULT_GROUPS_FILE, host_file_path=host_file_path,
-                           group_file_path=group_file_path, action_file_path=action_file_path, max_workers=max_workers)
+    template_data = {
+        'default_hosts_file': DEFAULT_HOSTS_FILE,
+        'default_action_file': DEFAULT_ACTION_FILE,
+        'default_groups_file': DEFAULT_GROUPS_FILE,
+        'host_file_path': host_file_path,
+        'group_file_path': group_file_path,
+        'action_file_path': action_file_path,
+        'max_workers': max_workers,
+        'error_messages': error_messages
+    }
+    return render_template('index.html', **template_data)
 
 def open_browser(host, port):
     webbrowser.open_new(f'http://{host}:{port}/')
